01e1b7592335e5672c39a7f5f38b15325b205bad
Ben Kurtin <netmail4ben@gmail.com>
2017-12-23 20:19:36 -0800

    Worked on GUI code

--- .idea/src/gui/GUI.java
+++ .idea/src/gui/GUI.java
@@ -24,50 +24,60 @@ import javafx.stage.Stage;
 
 public class GUI extends Application
 {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(null, 0, 0);
         Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
-        double win_border_thickness = 5;
-        double win_title_bar_height = 5;
+        double win_border_thickness = 10;
+        double win_title_bar_height = 50;
 
-        // Load the Image
-        Image image = new Image("file:\\C:\\Users\\Gamerverise Q J\\IdeaProjects\\JavaFXMessingAround\\src\\FRC_2017_RoboMap.png");
-        double map_aspect_ratio = image.getWidth() / image.getHeight();
-        double screen_aspect_ratio = primaryScreenBounds.getWidth() / primaryScreenBounds.getHeight();
-        double console_aspect_ratio = (map_aspect_ratio * 1.33) / (image.getHeight() + (0.33 * map_aspect_ratio));
-        double max_pane_width = primaryScreenBounds.getWidth() - 2*win_border_thickness;
-        double max_pane_height = scene.getY() - win_title_bar_height - win_border_thickness;
+        double max_pane_width = primaryScreenBounds.getWidth() - 2 * win_border_thickness;
+        double max_pane_height = primaryScreenBounds.getHeight() - win_title_bar_height - win_border_thickness;
 
-        double width = 0;
-        double height = primaryScreenBounds.getHeight() * Math.round(image.getHeight() / 100);
+        Image map_image = new Image("file:\\C:\\Users\\Gamerverise Q J\\IdeaProjects\\FRC_2018\\FRC_2018\\FRC_2017_RoboMap.png");
 
-        // Create the Canvas
-        Canvas map = new Canvas(width, height);
+        double map_aspect_ratio = map_image.getWidth() / map_image.getHeight();
+        double console_aspect_ratio = (map_aspect_ratio * 1.33) / (map_image.getHeight() + (0.33 * map_aspect_ratio));
+        double max_pane_aspect_ratio = max_pane_width/max_pane_height;
 
-        // Get the graphics context of the console
-        GraphicsContext map_gc = map.getGraphicsContext2D();
+        double console_width;
+        double console_height;
 
-        // Draw the Image
-        map_gc.drawImage(image, 0, 0, width, height);
+        if (console_aspect_ratio >= max_pane_aspect_ratio) {
+            console_height = max_pane_height;
+            console_width = console_aspect_ratio * console_height;
+
+        }
+        else {
+            console_width = max_pane_width;
+            console_height = console_width / console_aspect_ratio;
+        }
+
+        double map_width = console_width / 1.33;
+        double map_height = map_width / map_aspect_ratio;
+
         /*
-        MenuBar menuBar = new MenuBar();
 
-        Menu menuGame = new Menu("Game");
-        MenuItem auto = new MenuItem("Start Autonomous");
-        menuGame.getItems().addAll(auto);
+        stage (Stage)
+            scene (Scene)
+                pane (Pane)
+                    map (Canvas)
+                    controls (Rectangle)
+                    data (Rectangle)
+                    robot (Rectangle)
 
-        Menu menuSettings = new Menu("Settings");
-        MenuItem setSize = new MenuItem("Change Resolution");
-        setSize.setOnAction((ActionEvent t) -> {
-             = 2;
-            console.setWidth(width * );
-            console.setHeight(height * );
-            gc.drawImage(image, 0, 0, width * , height * );
-        });
-        menuSettings.getItems().addAll(setSize);
+         */
 
-        menuBar.getMenus().addAll(menuGame, menuSettings); */
+        // Need to calcuate the right size of the robot
+
+        // Real dimensions in feet
+        double field_height = 40;
+        double field_width = 40 * map_aspect_ratio;
+        double robot_width = 3;
+        double robot_height = 3;
 
         Rectangle robot = new Rectangle(150, 40, 50, 50);
         robot.setArcHeight(15);
@@ -75,74 +85,29 @@ public class GUI extends Application
         robot.setStroke(Color.BLUE);
         robot.setFill(Color.SLATEGRAY);
 
-        Rectangle controls = new Rectangle(0 + width, 0, width * 0.33, height);
-        controls.setFill(Color.CADETBLUE);
+        // Need data rect size
 
-        Rectangle data = new Rectangle(0, 0 + height, width + (width * 0.33), height * 0.33);
+        Rectangle data = new Rectangle(0, 0 + map_height, map_width + (map_width * 0.33), map_height * 0.33);
         data.setFill(Color.CRIMSON);
 
-        // Create the Pane
-        Pane root = new Pane(map, robot, controls, data);
+        // Need data rect size
 
-        // Add the Canvas to the Pane
-        //root.getChildren().add(console);
-        // root.getChildren().add(menuBar);
-        // Create the Scene
+        Rectangle controls = new Rectangle(0 + map_width, 0, map_width * 0.33, map_height);
+        controls.setFill(Color.CADETBLUE);
 
-        // Add the Scene to the Stage
+        // Need map size
+        Canvas map = new Canvas(console_width, console_height);
+        GraphicsContext map_gc = map.getGraphicsContext2D();
+        map_gc.drawImage(map_image, 0, 0, map_width, map_height);
+
+        Pane root = new Pane(map, controls, data, robot);
+
+        Scene scene = new Scene(root, max_pane_width, max_pane_height);
+
         stage.setScene(scene);
         stage.setResizable(false);
-        // Set the Title of the Stage
         stage.setTitle("ROBOTS DON'T QUIT");
 
-        /*
-
-
-        //set Stage boundaries to visible bounds of the main screen
-        stage.setX(primaryScreenBounds.getMinX());
-        stage.setY(primaryScreenBounds.getMinY());
-        stage.setWidth(primaryScreenBounds.getWidth());
-        stage.setHeight(primaryScreenBounds.getHeight()); */
-        /*
-        System.out.println("Window Width: " + stage.getWidth());
-        System.out.println("Window Height: " + stage.getHeight());
-
-        root.setTranslateX((stage.getWidth() - ((mapRatio * 1.33) * (height + (1 / 3) * mapRatio))) / 2);
-        root.setTranslateY((stage.getHeight() - (height + (1 / 3) * mapRatio)) / 8); */
-
-        /* Rectangle bg = new Rectangle(0 - (stage.getWidth() - ((mapRatio * 1.33) * (height + (1 / 3) * mapRatio))) / 2, 0 - (stage.getHeight() - (height + (1 / 3) * mapRatio)) / 8, primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight());
-        root.getChildren().add(bg);
-        console.toFront();
-        robot.toFront();
-        controls.toFront();
-        data.toFront(); */
-
-/*
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP:    goNorth = true; break;
-                    case DOWN:  goSouth = true; break;
-                    case LEFT:  goWest  = true; break;
-                    case RIGHT: goEast  = true; break;
-                }
-            }
-        });
-
-        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP: goNorth = false; break;
-                    case DOWN: goSouth = false; break;
-                    case LEFT: goWest = false; break;
-                    case RIGHT: goEast = false; break;
-                }
-            }
-        }); */
-
-        // Display the Stage
         stage.show();
     }
 }

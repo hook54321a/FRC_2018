76e67583d5c68c3ba40fa6ea8944f1e5ab6e0d4b
Ben Kurtin <netmail4ben@gmail.com>
2018-01-10 14:34:19 -0800

    Incremental

--- .idea/src/gui/GUITest.java
+++ .idea/src/gui/GUITest.java
@@ -20,54 +20,7 @@ import java.io.FileNotFoundException;
 public class GUITest extends Application {
 
     static Background bg;
-
-    static class ControlPanelWidgetTest extends TitledPane {
-        TilePane tiles;
-
-        ModeToggleWidget mode;
-        Button bark_button;
-        Button water_button;
-
-        MediaPlayer bark_player;
-
-        public ControlPanelWidgetTest() {
-//            getStyleClass().addAll("AEMBOT", "AEMBOT_Framed_Group", "AEMBOT_ControlPanelWidget");
-            getStyleClass().addAll("yo");
-
-            setText("Controls");
-
-            tiles = new TilePane();
-            tiles.getStyleClass().addAll("AEMBOT", "AEMBOT_TilePane", "AEMBOT_ControlPanelWidget_TilePane");
-
-//            tiles.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
-            bg = tiles.getBackground();
-            bg = new Background(new BackgroundFill(Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY));
-
-            mode = new ModeToggleWidget();
-            bark_button = new Button("Bark!");
-            water_button = new Button("Water Challenge!");
-
- //           Region children[] = {mode, bark_button, water_button};
-            Region children[] = {bark_button};
-            tiles.setPrefRows(children.length);
-
-            tiles.getChildren().addAll(children);
-
-            setContent(tiles);
-        }
-
-        @Override
-        protected void layoutChildren() {
-            super.layoutChildren();
-
-            layoutInArea(tiles,
-                    0, 0,
-                    getWidth(), getHeight(),
-                    0, HPos.CENTER, VPos.CENTER);
-
-            tiles.layout();
-        }
-    }
+    static int __x;
 
     @Override
     public void start(Stage primaryStage) {
@@ -92,7 +45,7 @@ public class GUITest extends Application {
 
         Scene scene = new Scene(root, 300, 250);
 
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Hellyo World!");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -102,4 +55,53 @@ public class GUITest extends Application {
     {
         launch(args);
     }
+}
+
+class ControlPanelWidgetTest extends TitledPane {
+    TilePane tiles;
+
+    ModeToggleWidget mode;
+    Button bark_button;
+    Button water_button;
+
+    MediaPlayer bark_player;
+
+    public ControlPanelWidgetTest() {
+//            getStyleClass().addAll("AEMBOT", "AEMBOT_Framed_Group", "AEMBOT_ControlPanelWidget");
+        getStyleClass().addAll("yo");
+
+        setText("Controls");
+
+        tiles = new TilePane();
+        tiles.getStyleClass().addAll("AEMBOT", "AEMBOT_TilePane", "AEMBOT_ControlPanelWidget_TilePane");
+
+//            tiles.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+        GUITest.bg = tiles.getBackground();
+        GUITest.bg = new Background(new BackgroundFill(Color.CORNFLOWERBLUE, CornerRadii.EMPTY, Insets.EMPTY));
+        GUITest.__x = 5;
+
+        mode = new ModeToggleWidget();
+        bark_button = new Button("Bark! Test");
+        water_button = new Button("Water Challenge!");
+
+        //           Region children[] = {mode, bark_button, water_button};
+        Region children[] = {bark_button};
+        tiles.setPrefRows(children.length);
+
+        tiles.getChildren().addAll(children);
+
+        setContent(tiles);
+    }
+
+//        @Override
+//        protected void layoutChildren() {
+//            super.layoutChildren();
+//
+//            layoutInArea(tiles,
+//                    0, 0,
+//                    getWidth(), getHeight(),
+//                    0, HPos.CENTER, VPos.CENTER);
+//
+//            tiles.layout();
+//        }
 }
